5a4e333e169619b6a5162593a120c80caca1dd64
SASHiBHUMARAJU2 <sasi.654forever@gmail.com>
2022-12-14 23:00:25 +0530

    updated

--- src/PlotsBody.js
+++ src/PlotsBody.js
@@ -1,27 +1,25 @@
 import React from "react";
 import map from './assets/map2.jpg'
-import ImageMapper from 'react-image-mapper';
+import ImageMapperWeb from 'react-image-mapper';
+import ImageMapperMob from 'react-img-mapper';
+import {isMobile} from 'react-device-detect';
 import plotsDataServices
  from "./services/plots-data.services";
  import PlotCard from './components/PlotCard.js'
 import './PlotsBody.css'
 import A from './list.js'
 import PrismaZoom from 'react-prismazoom'
-import { MapInteractionCSS } from 'react-map-interaction';
-import {TransformComponent, TransformWrapper} from 'react-zoom-pan-pinch'
 import Compass from "./components/Compass";
 import Pointer from "./components/Pointer";
-import close from './assets/close.svg'
+import close from './assets/close.svg';
+
+
+
 
 class MyMap extends React.Component {
 
-        static defaultProps ={
-            times : true
-        }
-
         constructor(props){
             super(props);
-        
             this.state = {
                 currentData : {},
                 active:false,
@@ -29,101 +27,33 @@ class MyMap extends React.Component {
             }
         }
   
-        closecard = () => {
+    closecard = () => {
             this.setState({
                 active : false
-            })
-        }
+            })}
     
-    handleClick=( obj)=>{
-       console.log("entered")
+    handleClick = ( obj )=>{
         this.setState({currentData : obj,active:true,showCard:true});
-        console.log(obj.name);
           }
 
 
     render(){
         const closeButton =  <div className="close" onClick={this.closecard}><img className="close-body" src={close}></img></div>
         const card =<> <PlotCard PlotNum={this.state.currentData.name} Available={this.state.currentData.available} Dimension={this.state.currentData.dimension} Size={this.state.currentData.size} Facing={this.state.currentData.facing} Slidein={this.state.active}></PlotCard> {this.state.active? closeButton : ''} </>;
+        const mapper = isMobile?  <ImageMapperMob src={map} lineWidth={0.00001}   width={1125} imgWidth={12413} map={{ name: "my-map",areas: [...A.map((v)=>{let c = (v.available)? 'rgba(50, 205, 50, 0.25)' : 'rgba(220, 20, 60, 0.25)'; return {...v,preFillColor: c}       })]    }}  onClick={area => this.handleClick(area)}/>   :         <ImageMapperWeb src={map} lineWidth={0.00001}   width={1125} imgWidth={12413} map={{ name: "my-map",areas: [...A.map((v)=>{let c = (v.available)? 'rgba(50, 205, 50, 0.25)' : 'rgba(220, 20, 60, 0.25)'; return {...v,preFillColor: c}       })]    }}  onClick={area => this.handleClick(area)}/> ;
+        
         return(
             <div className="PlotsBody">
-            <div className="container"    > 
-            
-                <div className="image-container"  >
-                {/* <TransformWrapper 
-            //   velocityAnimation={{disabled:true}}
-              pinch={{disabled:true}}
-                                    maxScale={25}
-                                    limitToBounds
-                                    centerOnInit
-                                    
-                                    wheel={{touchPadDisabled:true}}
-                                    centerZoomedOut>
-                      */}
-                            {/* <MapInteractionCSS
-                           value={this.state.value}
-                       
-                           onChange={(value) =>{
-                            if(value.scale < 5 && this.state.prevScale > value.scale)
-                            {
-                                // this.setState(
-                          
-                                //     { value: {...value,translation: { x: 0, y: 0 },scale:1}})
-                            }
-
-                           
-                           else  if(value.scale === 1)
-                             {
-                                this.setState(
-                          
-                                    { value: {...value,translation: { x: 0, y: 0 }}})
-                             }
-                           
-                             else
-                            {
-                                this.setState(
-                          
-                                    { value: {...value}})
-                            }
-                            this.state.prevScale = value.scale;
-                           }
-                           } 
-                             disablePan={false}  maxScale={15} minScale={1} 
-         >
-        */}
-            {/* <TransformComponent
-                        > */}
+                <div className="container"    > 
+                    <div className="image-container"  >
                         <div className="inner-image-container">
-                        <PrismaZoom maxZoom={18} >
-
-        <ImageMapper src={map}
-               
-                lineWidth={0.00001} 
-                width={1125} 
-                imgWidth={12413} 
-                map={{ name: "my-map",
-                       areas: [
-                            ...A.map((v)=>{
-                                let c = (v.available)? 'rgba(50, 205, 50, 0.25)' : 'rgba(220, 20, 60, 0.25)'
-                            return {...v,preFillColor: c}
-                                         })]    }}  
-    	onClick={area => this.handleClick(area)}
-    />           
-     {/* </TransformComponent> */}
-   
-              {/* </MapInteractionCSS> */}
-             
-              {/* </TransformWrapper> */}
-              </PrismaZoom>
-              </div>
-             <Compass></Compass>
-             <Pointer></Pointer>
-              {this.state.showCard? card : ''} 
-              </div>
-              
-              </div>
-          
-             
+                            <PrismaZoom maxZoom={18} >{mapper}  </PrismaZoom>
+                        </div>
+                        <Compass></Compass>
+                        <Pointer></Pointer>
+                        {this.state.showCard? card : ''} 
+                    </div>
+                </div>
             </div>
         );
     }
